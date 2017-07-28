bfdfba8aa69101db8684ab45acb2d551f233e0cc
Donnrey <donnreytan@gmail.com>
2017-07-28 12:25:19 +0800

    first commit

--- server.js
+++ server.js
@@ -10,14 +10,15 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, 'public')));
 //express routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'login form'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.get('/about', function(req, res){
-  res.sendFile(path.join(__dirname, 'login form'));
+  res.sendFile(path.join(__dirname, 'registration.html'));
 });
-app.get('*', function(req, res){
-  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+app.get('/about', function(req, res){
+  res.sendFile(path.join(__dirname, 'Edit User profile.html'));
 });
+
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
